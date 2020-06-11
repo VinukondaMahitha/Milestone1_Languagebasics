686e1461ea9b928c8df48346ee3c7616b8b2478d
Lenovo <Lenovo@DESKTOP-ESKVQ4N>
2020-06-11 18:34:51 +0530

    Ex-4

---
+++ Milestone1/src/com/wipro/flowcontrol/Ex4B.java
@@ -0,0 +1,9 @@
+package com.wipro.flowcontrol;
+
+public class Ex4B {
+public static void main(String[] args) {
+	int a=Integer.parseInt(args[0]);
+	int b=Integer.parseInt(args[1]);
+		System.out.println(a%10==7 && b%10==7);
+}
+}

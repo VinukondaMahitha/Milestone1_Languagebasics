88ed367b868f4fffbc86f7d3508f9b61cb1822f2
Lenovo <Lenovo@DESKTOP-ESKVQ4N>
2020-06-12 08:42:05 +0530

    Ex-8(Switch case)

---
+++ Milestone1/src/com/wipro/flowcontrol/Ex8.java
@@ -0,0 +1,31 @@
+package com.wipro.flowcontrol;
+
+public class Ex8 {
+public static void main(String[] args) {
+	String a=args[0];
+	char s=a.charAt(0);
+	switch(s)
+	{
+	case 'R':
+		System.out.println("Red");
+		break;
+	case 'B':
+		System.out.println("Blue");
+		break;
+	case 'G':
+		System.out.println("Green");
+		break;
+	case 'Y':
+		System.out.println("Yellow");
+		break;
+	case 'O':
+		System.out.println("Orange");
+		break;
+	case 'W':
+		System.out.println("White");
+		break;
+	default :
+		System.out.println("Invalid color");
+	}
+}
+}

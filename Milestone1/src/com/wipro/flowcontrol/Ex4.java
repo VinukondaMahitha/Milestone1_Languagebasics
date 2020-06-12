bed0879ca964f085d53ab67858da53cbe51b8b48
Lenovo <Lenovo@DESKTOP-ESKVQ4N>
2020-06-12 07:51:38 +0530

    Ex-4(Alphabetical order)

---
+++ Milestone1/src/com/wipro/flowcontrol/Ex4.java
@@ -0,0 +1,19 @@
+package com.wipro.flowcontrol;
+
+public class Ex4 
+{
+	public static void main(String[] args) {
+		char s1='b';
+		char s2='a';
+		if(s2 > s1)
+		{
+			System.out.println(s1+","+s2);
+		}
+		else
+		{
+			System.out.println(s2+","+s1);
+		}
+		
+	}
+
+}
